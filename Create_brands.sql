bd398c756f7ab472e46a716ada5c4c97f0f9d2cd
erickjones2 <53021426+erickjones2@users.noreply.github.com>
2023-10-25 13:51:07 -0500

    Update Create_brands.sql

--- Create_brands.sql
+++ Create_brands.sql
@@ -1,4 +1,4 @@
--- 10/25/2023 change 2
+-- 10/25/2023 change 3
 if not exists (select * from sys.tables t join sys.schemas s on (t.schema_id = s.schema_id) where s.name = 'production' and t.name = 'brands') 	
 CREATE TABLE production.brands (
 	brand_id INT IDENTITY (1, 1) PRIMARY KEY,
